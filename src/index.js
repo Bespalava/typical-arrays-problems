7577af2aeb9a87eae6c23371bd7e1bc0e0465169
ybespalava <ybespalava@gmail.com>
2021-03-12 10:17:37 +0300

    fix: modify min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 exports.min = function min (array) {  
-  return Math.min(array);
+    if (arguments.length === 0 || array.length === 0) return 0;
+    a = Math.min(array);
+    return a[0];
 }
 
 exports.max = function max (array) {
